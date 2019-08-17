516d739e1838c3e62ee4a6d383401a368d1baf9c
a.pinto <alessandro.pinto@ext.europassistance.it>
2019-08-17 11:27:12 +0200

    implement help command

--- WiredBrainCofee.ShopInfoTool/Program.cs
+++ WiredBrainCofee.ShopInfoTool/Program.cs
@@ -2,11 +2,29 @@
 
 namespace WiredBrainCofee.ShopInfoTool
 {
+    using WiredBrainCofee.DataAccess.Model;
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wired Brain!");
+
+            var coffeeShopDataProvider = new CoffeShopDataProvider();
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> available coffeeshop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
         }
     }
 }
