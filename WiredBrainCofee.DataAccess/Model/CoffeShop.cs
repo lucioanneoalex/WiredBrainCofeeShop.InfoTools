1691e07d135b7efc8662d63c775f82d2fb478914
a.pinto <alessandro.pinto@ext.europassistance.it>
2019-08-17 11:25:48 +0200

    implement help command

---
+++ WiredBrainCofee.DataAccess/Model/CoffeShop.cs
@@ -0,0 +1,8 @@
+﻿namespace WiredBrainCofee.DataAccess.Model
+{
+    public class CoffeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStocksInKg { get; set; }
+    }
+}
---
+++ WiredBrainCofee.DataAccess/Model/CoffeShopDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WiredBrainCofee.DataAccess.Model
+{
+    public class CoffeShopDataProvider
+    {
+        public IEnumerable<CoffeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeShop { Location = "Frankfurt", BeansInStocksInKg = 107 };
+            yield return new CoffeShop { Location = "Feiburg", BeansInStocksInKg = 20 };
+            yield return new CoffeShop { Location = "Munich", BeansInStocksInKg = 40 };
+        }
+    }
+}
